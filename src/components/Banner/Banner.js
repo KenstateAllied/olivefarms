240d5aacb6016c4aff39411e63bc13c83f1b7d54
Sam Muttai <muttaisammy@gmail.com>
2025-03-10 09:35:37 +0300

    m

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <div className={styles.imgbox}>
             <div className={`${styles.center} overflow-hidden`}>
                 <p data-aos='fade-down' className={styles.text}>We hatch and sell day old, one month chicks</p>
-                <button data-aos='fade-up' className={`btn custom_btn ${styles.btn}`}>Contact us and make your order 0713851554 PAYBILL NO 00000</button>
+                <button data-aos='fade-up' className={`btn custom_btn ${styles.btn}`}>Contact us and make your order 0713851554/ 0724052417</button>
                
             </div>
           
